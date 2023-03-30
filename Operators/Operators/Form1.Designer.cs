a8046bd2c45f4f494923a343920606bd7f6cacc4
Anastas Acevski <123334501+Ace9000@users.noreply.github.com>
2023-03-31 00:06:21 +0200

    Add files via upload

---
+++ Operators/Operators/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Operators
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.txtRezultat = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMnozenje = new System.Windows.Forms.Button();
+            this.btnDelenje = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(92, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Second Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(144, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result:";
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(228, 49);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(133, 22);
+            this.txtFirstNumber.TabIndex = 3;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(228, 99);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(133, 22);
+            this.txtSecondNumber.TabIndex = 4;
+            // 
+            // txtRezultat
+            // 
+            this.txtRezultat.Location = new System.Drawing.Point(228, 154);
+            this.txtRezultat.Name = "txtRezultat";
+            this.txtRezultat.Size = new System.Drawing.Size(133, 22);
+            this.txtRezultat.TabIndex = 5;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPlus.Location = new System.Drawing.Point(123, 216);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(62, 54);
+            this.btnPlus.TabIndex = 6;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMinus.Location = new System.Drawing.Point(203, 216);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(62, 54);
+            this.btnMinus.TabIndex = 7;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMnozenje
+            // 
+            this.btnMnozenje.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMnozenje.Location = new System.Drawing.Point(303, 216);
+            this.btnMnozenje.Name = "btnMnozenje";
+            this.btnMnozenje.Size = new System.Drawing.Size(62, 54);
+            this.btnMnozenje.TabIndex = 8;
+            this.btnMnozenje.Text = "*";
+            this.btnMnozenje.UseVisualStyleBackColor = true;
+            this.btnMnozenje.Click += new System.EventHandler(this.btnMnozenje_Click);
+            // 
+            // btnDelenje
+            // 
+            this.btnDelenje.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelenje.Location = new System.Drawing.Point(388, 216);
+            this.btnDelenje.Name = "btnDelenje";
+            this.btnDelenje.Size = new System.Drawing.Size(62, 54);
+            this.btnDelenje.TabIndex = 9;
+            this.btnDelenje.Text = "/";
+            this.btnDelenje.UseVisualStyleBackColor = true;
+            this.btnDelenje.Click += new System.EventHandler(this.btnDelenje_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 397);
+            this.Controls.Add(this.btnDelenje);
+            this.Controls.Add(this.btnMnozenje);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.txtRezultat);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.TextBox txtRezultat;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMnozenje;
+        private System.Windows.Forms.Button btnDelenje;
+    }
+}
+
---
+++ Operators/Operators/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Operators
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            Int32 txtFirstNumber;
+            Int32 txtSecondNumber;
+            Int32 txtRezultat;
+
+           Int32.TryParse(this.txtFirstNumber.Text, out txtFirstNumber);
+           Int32.TryParse(this.txtSecondNumber.Text, out txtSecondNumber);
+
+            txtRezultat = txtFirstNumber + txtSecondNumber;
+            this.txtRezultat.Text = txtRezultat.ToString();
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            Int32 txtFirstNumber;
+            Int32 txtSecondNumber;
+            Int32 txtRezultat;
+
+            Int32.TryParse(this.txtFirstNumber.Text, out txtFirstNumber);
+            Int32.TryParse(this.txtSecondNumber.Text, out txtSecondNumber);
+
+            txtRezultat = txtFirstNumber - txtSecondNumber;
+            this.txtRezultat.Text = txtRezultat.ToString();
+        }
+
+        private void btnMnozenje_Click(object sender, EventArgs e)
+        {
+            Int32 txtFirstNumber;
+            Int32 txtSecondNumber;
+            Int32 txtRezultat;
+
+            Int32.TryParse(this.txtFirstNumber.Text, out txtFirstNumber);
+            Int32.TryParse(this.txtSecondNumber.Text, out txtSecondNumber);
+
+            txtRezultat = txtFirstNumber * txtSecondNumber;
+            this.txtRezultat.Text = txtRezultat.ToString();
+        }
+
+        private void btnDelenje_Click(object sender, EventArgs e)
+        {
+            Int32 txtFirstNumber;
+            Int32 txtSecondNumber;
+            Int32 txtRezultat;
+
+            Int32.TryParse(this.txtFirstNumber.Text, out txtFirstNumber);
+            Int32.TryParse(this.txtSecondNumber.Text, out txtSecondNumber);
+
+            txtRezultat = txtFirstNumber / txtSecondNumber;
+            this.txtRezultat.Text = txtRezultat.ToString();
+        }
+    }
+}
